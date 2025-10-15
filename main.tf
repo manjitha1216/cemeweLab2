7b41c9a68146dd928caed4e37da8cc56f0d03555
m.manchanayake <m.manchanayake@cyberdefense.global>
2025-10-15 13:11:42 +0300

    s3 deployment using terraform

---
+++ main.tf
@@ -0,0 +1,32 @@
+#########################################################
+# Lab 2 - Create Simple S3 Bucket using Terraform
+#########################################################
+
+provider "aws" {
+  region  = "eu-north-1"
+  profile = "cemewe"
+}
+
+#########################################################
+# Create S3 bucket
+#########################################################
+resource "aws_s3_bucket" "lab2_bucket" {
+  bucket = "lab2-${var.student_name}-bucket"  # unique bucket name
+
+  tags = {
+    Name  = "lab2-${var.student_name}"
+    Owner = var.student_name
+    Lab   = "2"
+  }
+}
+
+#########################################################
+# Outputs
+#########################################################
+output "bucket_name" {
+  value = aws_s3_bucket.lab2_bucket.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.lab2_bucket.arn
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+###########################################
+# Variables for Lab 2
+###########################################
+
+variable "student_name" {
+  description = "Unique student identifier"
+  type        = string
+  default     = "manjitha"  # <-- Each student updates this value
+}
